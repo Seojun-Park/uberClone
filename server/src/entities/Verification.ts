24778ca1ae8496d92711ed929b220d544cef7c5f
Jinchul park <jinchul112@gmail.com>
2020-11-30 19:40:50 +0100

    u

--- server/src/entities/Verification.ts
+++ server/src/entities/Verification.ts
@@ -1,6 +1,7 @@
 import { verificationTarget } from "src/types/types";
 import {
   BaseEntity,
+  BeforeInsert,
   Column,
   CreateDateColumn,
   Entity,
@@ -8,11 +9,14 @@ import {
   UpdateDateColumn
 } from "typeorm";
 
+const PHONE = "PHONE";
+const EMAIL = "EMAIL";
+
 @Entity()
 class Verification extends BaseEntity {
   @PrimaryColumn() id: number;
 
-  @Column({ type: "text", enum: ["PHONE", "EMAIL"] })
+  @Column({ type: "text", enum: [PHONE, EMAIL] })
   target: verificationTarget;
 
   @Column({ type: "text" })
@@ -29,6 +33,15 @@ class Verification extends BaseEntity {
 
   @UpdateDateColumn()
   updatedAt: string;
+
+  @BeforeInsert()
+  createKey(): void {
+    if (this.target === PHONE) {
+      this.key = Math.floor(Math.random() * 100000).toString();
+    } else if (this.target === EMAIL) {
+      this.key = Math.random().toString(36).substr(2);
+    }
+  }
 }
 
 export default Verification;
