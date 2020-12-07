dd12f29a1d2c8f1c7ad0207d6c07118cc934b8d4
Jinchul park <jinchul112@gmail.com>
2020-12-07 13:35:28 +0100

    u

--- server/src/utils/decodeJWT.ts
+++ server/src/utils/decodeJWT.ts
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
-import Member from "../entities/Member";
+import User from "../entities/User";
 
 const decodeJWT = async (token: string): Promise<any> => {
   try {
     const decoded:any = jwt.verify(token, process.env.JWT_TOKEN || "");
     const { id } = decoded;
-    const member = await Member.findOne({ id });
+    const member = await User.findOne({ id });
     return member;
   } catch (error) {
     return undefined;
