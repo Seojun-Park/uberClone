d4be0c8ead5d4c65bdb265235704f74488aa54b2
Jinchul park <jinchul112@gmail.com>
2020-12-07 14:01:26 +0100

    u

---
+++ server/src/api/User/EmailAuth/EmailAuth.resolvers.ts
@@ -0,0 +1,107 @@
+import User from "../../../entities/User";
+import Verification from "../../../entities/Verification";
+import {
+  EmailSignInMutationArgs,
+  EmailSignUpMutationArgs,
+  EmailSignInResponse,
+  EmailSignUpResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import createJWT from "../../../utils/createJWT";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignIn: async (
+      _,
+      args: EmailSignInMutationArgs
+    ): Promise<EmailSignInResponse> => {
+      const { email, password } = args;
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return {
+            ok: false,
+            error: "No user found with that email",
+            token: null
+          };
+        }
+        const checkPassword = await user.comparePassword(password);
+        if (checkPassword) {
+          const token = createJWT(user.id);
+          return {
+            ok: true,
+            error: null,
+            token
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Wrong password",
+            token: null
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+    },
+    EmailSignUp: async (
+      _,
+      args: EmailSignUpMutationArgs
+    ): Promise<EmailSignUpResponse> => {
+      const { email } = args;
+      console.log("test");
+      try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return {
+            ok: false,
+            error: "You should log in instead",
+            token: null
+          };
+        } else {
+          const phoneVerification = await Verification.findOne({
+            payload: args.phoneNumber,
+            verified: true
+          });
+          if (phoneVerification) {
+            const newUser = await User.create({ ...args }).save();
+            if (newUser.email) {
+              const emailVerification = await Verification.create({
+                payload: newUser.email,
+                target: "EMAIL"
+              });
+              await sendVerificationEmail(
+                newUser.fullName,
+                emailVerification.key
+              );
+            }
+            const token = createJWT(newUser.id);
+            return {
+              ok: true,
+              error: null,
+              token
+            };
+          } else {
+            return {
+              ok: false,
+              error: "You haven't verified your phone number",
+              token: null
+            };
+          }
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+    }
+  }
+};
+export default resolvers;
