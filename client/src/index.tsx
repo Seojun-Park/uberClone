d26913420840b488ea6a05b2872aa8fa7e665a0f
Jinchul park <jinchul112@gmail.com>
2020-12-10 21:58:10 +0100

    u

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/client'
+import App from './App';
+import client from './apollo'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
