3d362cc82be36f5d16d3a67e4303cceed1857ebf
Jinchul park <jinchul112@gmail.com>
2020-12-14 16:56:07 +0100

    u

--- client/src/Routes/EditAccount/EditAccountContainer.tsx
+++ client/src/Routes/EditAccount/EditAccountContainer.tsx
@@ -1,12 +1,23 @@
+import { useMutation } from '@apollo/client';
 import React from 'react';
 import useInput from '../../Hooks/useInput';
 import EditAccountPresenter from './EditAccountPresenter'
+import { UPDATE_PROFILE } from './EditAccountQueries'
+import { updateProfile, updateProfileVariables } from '../../types/api'
 
 const EditAccountContainer = () => {
     const email = useInput("");
     const firstName = useInput("")
     const lastName = useInput("")
     const profilePhoto = useInput("")
+    const [updateProfileMutation] = useMutation<updateProfile, updateProfileVariables>(UPDATE_PROFILE, {
+        variables: {
+            email,
+            firstName,
+            lastName,
+            profilePhoto
+        }
+    })
 
     return (
         <EditAccountPresenter
