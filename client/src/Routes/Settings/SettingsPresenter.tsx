050d0aa93b2fc4a0e4b81cfaf452bb5adf01d2e8
Jinchul park <jinchul112@gmail.com>
2020-12-15 00:47:51 +0100

    u

--- client/src/Routes/Settings/SettingsPresenter.tsx
+++ client/src/Routes/Settings/SettingsPresenter.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../../Components/Header'
+import * as S from './SettingsStyles'
 
 const SettingsPresenter = () => {
     return (
-        <>
-            lala
-        </>
+        <S.Container>
+            <Header title="Account Settings" backTo="/" />
+        </S.Container>
     )
 }
 
---
+++ client/src/Routes/Settings/SettingsStyles.ts
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Image = styled.img`
+  height: 60px;
+  width: 60px;
+  border-radius: 50%;
+`;
+
+export const GridLink = styled(Link)`
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-gap: 10px;
+  margin-bottom: 10px;
+`;
+
+export const Keys = styled.div``;
+
+export const Key = styled.span`
+  display: block;
+  margin-bottom: 5px;
+`;
+
+export const FakeLink = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+export const SLink = styled(Link)`
+  display: block;
+  text-decoration: underline;
+  margin: 20px 0px;
+`;
