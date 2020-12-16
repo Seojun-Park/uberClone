7d6ba2a73c78fa1904d96f1b692e3600c18652e8
Jinchul park <jinchul112@gmail.com>
2020-12-16 14:43:57 +0100

    u

--- client/src/Routes/Settings/SettingsContainer.tsx
+++ client/src/Routes/Settings/SettingsContainer.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useQuery } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
+import { ME } from '../Home/HomeQueries';
 import SettingsPresenter from './SettingsPresenter'
 
 const SettingsContainer = () => {
+    const [user, setUser] = useState()
+    const [loading, setLoading] = useState(true)
+    useQuery(ME, { onCompleted: v => setUser(v.Me.user) })
+
+    useEffect(() => {
+        if (user) {
+            setLoading(false);
+        }
+    }, [setLoading, user])
+
+    if (loading) {
+        <>
+            loading...
+        </>
+    }
+    console.log(user)
     return (
         <SettingsPresenter />
     )
--- client/src/Routes/Settings/SettingsPresenter.tsx
+++ client/src/Routes/Settings/SettingsPresenter.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Header from '../../Components/Header'
+import PlacePresenter from '../../Components/Place/Place';
 import * as S from './SettingsStyles'
 
 const SettingsPresenter = () => {
     return (
-        <S.Container>
+        <>
+            <Helmet>Settings</Helmet>
             <Header title="Account Settings" backTo="/" />
-        </S.Container>
+            <S.Container>
+                <S.GridLink to={"/editAccount"}>
+
+                </S.GridLink>
+                <PlacePresenter fav={false} name="Home" address="Paris" />
+                <PlacePresenter fav={false} name="Home" address="Paris" />
+                <PlacePresenter fav={false} name="Home" address="Paris" />
+                <S.SLink to="/place">Go to Places</S.SLink>
+                <S.FakeLink>Log Out</S.FakeLink>
+            </S.Container>
+        </>
     )
 }
 
