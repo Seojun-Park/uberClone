a6e9cb127f18d00d9ff2da22cbc395db2f792f72
Jinchul park <jinchul112@gmail.com>
2020-12-21 20:09:22 +0100

    u

--- server/src/api/User/GetNearbyDrivers/GetNearbyDrivers.resovers.ts
+++ server/src/api/User/GetNearbyDrivers/GetNearbyDrivers.resovers.ts
@@ -5,26 +5,53 @@ import { Resolvers } from "../../../types/resolvers";
 import authResolver from "../../../utils/authResolver";
 
 const resolvers: Resolvers = {
+  //   Query: {
+  //     GetNearbyDrivers: authResolver(
+  //       async (_, __, { req }): Promise<GetNearbyDriversResponse> => {
+  //         const user: User = req.user;
+  //         const { lastLat, lastLng } = user;
+  //         try {
+  //           const drivers: User[] = await getRepository(User).find({
+  //             isDriving: true,
+  //             lastLat: Between(lastLat - 0.05, lastLat + 0.05),
+  //             lastLng: Between(lastLng - 0.05, lastLng + 0.05)
+  //           });
+  //           return {
+  //             ok: true,
+  //             err: null,
+  //             drivers
+  //           };
+  //         } catch (err) {
+  //           return {
+  //             ok: false,
+  //             err: err.message,
+  //             drivers: null
+  //           };
+  //         }
+  //       }
+  //     )
+  //   }
+  // };
   Query: {
     GetNearbyDrivers: authResolver(
       async (_, __, { req }): Promise<GetNearbyDriversResponse> => {
         const user: User = req.user;
-        const { lastLat, lastLng } = user;
+        const { lastLng, lastLat } = user;
         try {
-          const drivers: User[] = await getRepository(User).find({
+          const drivers: User[] = await User.find({
             isDriving: true,
-            lastLat: Between(lastLat - 0.05, lastLat + 0.05),
-            lastLng: Between(lastLng - 0.05, lastLng + 0.05)
+            lastLng: Between(lastLng - 0.05, lastLng + 0.05),
+            lastLat: Between(lastLat - 0.05, lastLat + 0.05)
           });
           return {
             ok: true,
             err: null,
             drivers
           };
-        } catch (err) {
+        } catch (error) {
           return {
             ok: false,
-            err: err.message,
+            err: error.message,
             drivers: null
           };
         }
